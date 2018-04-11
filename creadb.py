7e2d5082527cb6b15ddfb988576bd147e2ca22e1
herodote2242 <matthias.gil@hotmail.fr>
2018-04-11 18:51:02 +0200

    writing the creation of the database and three tables : products_table, categories_table, store_table

--- creadb.py
+++ creadb.py
@@ -4,9 +4,9 @@
 import records
 
 class DatabaseCreator:
-    """This class has the responsibility of the database construction.
-    It constructs a DatabaseCreator object responsible of creating the tables
-    needed for the application.
+    """This class is responsible of the database construction, by constructing
+    a DatabaseCreator object responsible of creating the tables needed
+    for the application.
 
     ======
     Params:
@@ -15,18 +15,42 @@ class DatabaseCreator:
 
     def __init__(self, connection, dbname):
         self.dbname = connection
-        self.dbname.query = ("""CREATE DATABASE offdatabase (id INT PRIMARY_KEY AUTO_INCREMENT) / 
-        CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci';""")
+        self.dbname.query = ("""CREATE DATABASE offdatabase CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci';""")
+        self.db.query("""USE offdatabase;""")
+
 
     def create_products_table(self):
-        """Creates products in the database."""
-        self.db.query("""  """)
+        """Creates a table listing the products to be added to the database."""
+        self.db.query("""CREATE TABLE products_table (
+            code MEDIUMINT UNSIGNED NOT NULL PRIMARY KEY,
+            product_name VARCHAR(100) NOT NULL,
+            brands VARCHAR(50) NOT NULL,
+            ingredients_text TEXT,
+            lien_url VARCHAR(200),
+            store VARCHAR(150),
+            categories TEXT,
+            nutrition_grade_fr CHAR(1),
+            serving_size VARCHAR(50)
+            );
+            """)
 
     def create_categories_table(self):
-        """...."""
+        """Creates a table linking a product with one or several category/ies."""
+        self.db.query("""CREATE TABLE categories_table (
+            code MEDIUMINT UNSIGNED NOT NULL,
+            product_name VARCHAR(100),
+            categories TEXT
+            );
+            """)
 
     def create_store_table():
-        """   """
+        """Creates a table linking a product with one or several store(s)."""
+        self.db.query("""CREATE TABLE store_table (
+            code MEDIUMINT UNSIGNED NOT NULL,
+            product_name VARCHAR(100) NOT NULL,
+            store VARCHAR(150)
+            );
+            """)
         
 
 
