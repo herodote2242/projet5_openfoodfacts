36481e2b48961cf1691330f3e5dabf0088156655
herodote2242 <matthias.gil@hotmail.fr>
2018-04-13 19:28:32 +0200

    trying to fix a bug preventing to push on remote repo

--- creadb.py
+++ creadb.py
@@ -15,51 +15,79 @@ class DatabaseCreator:
 
     def __init__(self, connection):
         self.db = connection
-        self.db.query("""CREATE DATABASE IF NOT EXISTS projet5 CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci';""")
-        self.db.query("""USE projet5;""")
+        self.db.query("""CREATE DATABASE IF NOT EXISTS projet5 CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci'""")
+        self.db.query("""USE projet5""")
 
+
+    def clean_table(self):
+        """A function used to drop existing tables, in order to create 
+        new ones in case of a modification"""
+        self.db.query("""DROP TABLE product_category;
+            DROP TABLE product_store;
+            DROP TABLE category;
+            DROP TABLE product;
+            DROP TABLE store;
+            """)
 
     def create_product_table(self):
         """Creates a table listing the products to be added to the database."""
-        #self.db.query drop table product_category, pour ensuite drop category drop product => faire une méthode clean qui drop les tables dans l'ordre.
         self.db.query("""CREATE TABLE product (
             code INT UNSIGNED NOT NULL PRIMARY KEY,
             product_name VARCHAR(100) NOT NULL,
-            brands VARCHAR(50) NOT NULL,
-            lien_url VARCHAR(200),
+            brand VARCHAR(50) NOT NULL,
+            url_link VARCHAR(200),
             store VARCHAR(150),
             nutrition_grade_fr CHAR(1)
-            );
+            )
+            ENGINE=INNODB;
             """)
 
     def create_category_table(self):
         """Creates a table linking a product with one or several category/ies."""
         self.db.query("""CREATE TABLE category (
             id MEDIUMINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-            category_name VARCHAR(100),
-            );
+            category_name VARCHAR(100)
+            )
+            ENGINE=INNODB;
             """)
 
     def create_store_table(self):
-        """Creates a table linking a product with one or several store(s)."""
+        """Creates a table linking a product with one or several store/s."""
         self.db.query("""CREATE TABLE store (
             id MEDIUMINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
             store VARCHAR(150)
-            );
+            )
+            ENGINE=INNODB;
             """)
         
     def create_product_category_table(self):
+        """Creates a table joining the different products and related category/ies."""
         self.db.query("""CREATE TABLE product_category (
             id MEDIUMINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
             product_code INT UNSIGNED REFERENCES product(code),
             category_id MEDIUMINT UNSIGNED REFERENCES category(id)
-            );
+            )
+            ENGINE=INNODB;
             """)
 
+    def create_product_store_table(self):
+        """Create a table joining the different products and related store/s."""
+        self.db.query("""CREATE TABLE product_store (
+            id MEDIUMINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+            product_code INT UNSIGNED REFERENCES product(code),
+            store_id MEDIUMINT UNSIGNED REFERENCES store(id)
+            )
+            ENGINE=INNODB;
+            """)
+
+
+# Tests.
 if __name__ == "__main__":
     connection = records.Database("mysql+pymysql://root:rootroot@localhost/?charset=utf8mb4")
     creator = DatabaseCreator(connection)
-    creator.create_products_table()
+    creator.clean_table()
+    creator.create_product_table()
     creator.create_category_table()
     creator.create_store_table()
-
+    creator.create_product_category_table()
+    creator.create_product_store_table()
