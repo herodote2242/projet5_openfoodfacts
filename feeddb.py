22530f62475c5c65419ba3167778b9d004680915
herodote2242 <matthias.gil@hotmail.fr>
2018-04-16 16:06:23 +0200

    starting the instances responsible of database feeding

--- feeddb.py
+++ feeddb.py
@@ -2,10 +2,11 @@
 # -*- coding: Utf-8 -*
 
 import requests
+import records
 from pprint import pprint
 
 
-def feed_db():
+def feed_db(self):
     """This functions collects data from the Open Food Facts API
     according to the criteria and stores them into the database"""
     url="https://fr.openfoodfacts.org/cgi/search.pl"
@@ -24,7 +25,28 @@ def feed_db():
     req=requests.get(url, params=criteria)
     pprint(req.json())
 
+class DatabaseFeeder:
+    """ This class will integrate the results of the feed_db function into the
+    different tables created in the database : product, category, store."""
+
+    def __init__(self, connection, req):
+        self.db = connection
+        self.req = req
+
+    def product_manager(self):
+        self.db.query("""INSERT INTO 'product' (code, product_name, brand, url_links
+            store, nutrition_grade_fr) VALUES (self.req)""")
+
+    def category_manager(self):
+        self.db.query("""INSERT INTO 'category' (id, category_name) VALUES (self.req)""")
+
+    def store_manager(self):
+        self.db.query("""INSERT INTO 'store' (id, store) VALUES (self.req)""")
 
 #Tests:
 if __name__=="__main__":
-    feed_db()
+    feeder = DatabaseFeeder(connection, req)
+    feeder.product_manager()
+    feeder.category_manager()
+    feeder.store_manager()
+
