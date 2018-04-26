e7c8d1e41476b9245906a3bdf15848b8246072b5
herodote2242 <matthias.gil@hotmail.fr>
2018-04-26 11:04:48 +0200

    adding a class DatabaseFeeder

--- feeddb.py
+++ feeddb.py
@@ -4,48 +4,55 @@
 import requests
 import records
 from pprint import pprint
+import creadb
 
-
-def feed_db(self):
-    """This functions collects data from the Open Food Facts API
-    according to the criteria and stores them into the database"""
-    url="https://fr.openfoodfacts.org/cgi/search.pl"
-    criteria={
-        "action":"process",
-        "tagtype_0":"categories",
-        "tag_contains_0":"contains",
-        "tag_0":"produits laitiers",
-        "tagtype_1":"nutrition_grades",
-        "tag_contains_1":"contains",
-        "tag_1":"a",
-        "sort_by":"product_name",
-        "page_size":100,
-        "json":1
-    }
-    req=requests.get(url, params=criteria)
-    pprint(req.json())
 
 class DatabaseFeeder:
     """ This class will integrate the results of the feed_db function into the
     different tables created in the database : product, category, store."""
 
-    def __init__(self, connection, req):
+    def __init__(self, connection):
         self.db = connection
-        self.req = req
+
+    def feed_db(self):
+        """This functions collects data from the Open Food Facts API
+        according to the criteria"""
+        url="https://fr.openfoodfacts.org/cgi/search.pl"
+        criteria={
+            "action":"process",
+            "tagtype_0":"categories",
+            "tag_contains_0":"contains",
+            "tag_0":"produits laitiers",
+            "tagtype_1":"nutrition_grades",
+            "tag_contains_1":"contains",
+            "tag_1":"a",
+            "sort_by":"product_name",
+            "page_size":3,
+            "json":1
+        }
+        req=requests.get(url, params=criteria)
+        pprint(req.json())
 
     def product_manager(self):
-        self.db.query("""INSERT INTO 'product' (code, product_name, brand, url_links
-            store, nutrition_grade_fr) VALUES (self.req)""")
+        """The function is responsible of feeding the table "product" with the API's results"""
+        self.db.query("""INSERT INTO 'product' (code, product_name, brand, url_links,
+            store, nutrition_grade_fr) VALUES (:code, :product_name, :brand, :url_links,
+            :store, :nutrition_grade_fr);""")
 
     def category_manager(self):
-        self.db.query("""INSERT INTO 'category' (id, category_name) VALUES (self.req)""")
+        """The function is responsible of feeding the table "category" with the API's results"""
+        self.db.query("""INSERT INTO 'category' (id, category_name)
+            VALUES (:id, :category_name);""")
 
     def store_manager(self):
-        self.db.query("""INSERT INTO 'store' (id, store) VALUES (self.req)""")
+        """The function is responsible of feeding the table "store" with the API's results"""        
+        self.db.query("""INSERT INTO 'store' (id, store) VALUES (:id, :store);""")
 
 #Tests:
 if __name__=="__main__":
-    feeder = DatabaseFeeder(connection, req)
+    connection = records.Database("mysql+pymysql://root:root@localhost/?charset=utf8mb4")
+    feeder = DatabaseFeeder(connection)
+    feeder.feed_db()
     feeder.product_manager()
     feeder.category_manager()
     feeder.store_manager()
