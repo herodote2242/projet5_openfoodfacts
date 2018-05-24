f2c51af13341793443e2a8a5eab32ec0c197c7c2
Thierry Chappuis <thierry@chappuis.io>
2018-05-24 15:03:56 +0200

    Modification des paramètres de connexion

--- config.py
+++ config.py
@@ -3,5 +3,5 @@
 
 CATEGORIES_TO_RECOVER = ("purees", "pates a tartiner", "pizza", "poissons", "pain", "pates")
 NUMBER_OF_PRODUCTS = 1000
-DATABASE_URL = "mysql+mysqlconnector://tchappui:ellea.Mujigka.77@localhost/?charset=utf8mb4"
-DATABASE_NAME = "matthias_p5"
+DATABASE_URL = "mysql+mysqlconnector://root:root@localhost/?charset=utf8mb4"
+DATABASE_NAME = "projet5"
