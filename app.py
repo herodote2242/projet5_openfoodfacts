458b9d9d192bfde1f7e90e9fdc1410be2173b210
herodote2242 <matthias.gil@hotmail.fr>
2018-05-29 20:12:17 +0200

    adding the choices of the user while interacting with the app

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 # -*- coding: Utf-8 -*
 
 import records
+import config as c
 
 """This is the main application : it asks the user several questions
 and the user is able to answer in order to search products, see products, 
@@ -12,22 +13,25 @@ print("1 - Quel aliment souhaitez-vous remplacer ?")
 print("2 - Retrouver mes aliments substitués.")
 
 #User chooses 1:
-if int(input()) = 1:
+if input() == "1":
     print("Sélectionnez la catégorie :")
-    #pour n catégories, afficher un chiffre de 1 à n devant chacune
-    if int(input()) = #number of category:
-        print("Catégorie sélectionnée : "xxxx"")
-        print("Sélectionnez l'aliment :")
-        #pour n aliments, afficher un chiffre de 1 à n devant chacun
-        print("Aliment sélectionné : "yyyy"")
-        query("""USE projet5;""")
-        query("""SELECT * FROM product WHERE category = "xxxx" AND nutrition_grade_fr = 'a'
-            OR nutition_grade_fr = 'b' LIMIT 1""")
+    print(c.CATEGORIES_TO_RECOVER)
+    category_selected = int(input())
+    print("Catégorie sélectionnée : ", c.CATEGORIES_TO_RECOVER[category_selected])
+    print("Sélectionnez l'aliment :")
+
+
+    #print("Sélectionnez l'aliment :")
+    #pour n aliments, afficher un chiffre de 1 à n devant chacun
+    #print("Aliment sélectionné : "yyyy"")
+    #query("""USE projet5;""")
+    #query("""SELECT * FROM product WHERE category = "xxxx" AND nutrition_grade_fr = 'a'
+    #    OR nutition_grade_fr = 'b' LIMIT 3""")
 
 #User chooses 2:
-elif int(input()) = 2:
+elif input() == "2":
     print("Mes aliments substitués :")
-    query("""affichage de l'historique des aliments substitués""")
+    #faire une query("""affichage de l'historique des aliments substitués""")
 
 #User hits an other key:
 else:
--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: Utf-8 -*
 
-CATEGORIES_TO_RECOVER = ("puree", "pate a tartiner", "pizza", "poisson", "pain", "chocolat")
+CATEGORIES_TO_RECOVER = {1 : "puree", 2 : "pate a tartiner", 3 : "pizza", 4 : "poisson", 5 : "pain", 6 : "chocolat"}
 NUMBER_OF_PRODUCTS = 1000
 DATABASE_URL = "mysql+mysqlconnector://root:root@localhost/?charset=utf8mb4"
 DATABASE_NAME = "projet5"
