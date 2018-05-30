6bc49175cc4aec44db3a45933074026e7c4429e6
herodote2242 <matthias.gil@hotmail.fr>
2018-05-30 21:22:47 +0200

    adding the first steps of the app

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 # -*- coding: Utf-8 -*
 
 import records
+import requests
 import config as c
 
 """This is the main application : it asks the user several questions
@@ -11,25 +12,25 @@ save them into his favorites and see the list of favorites."""
 # User opens the app, the app shows:
 print("1 - Quel aliment souhaitez-vous remplacer ?")
 print("2 - Retrouver mes aliments substitués.")
+user_answer = input()
+connection = records.Database(c.DATABASE_URL)
 
 #User chooses 1:
-if input() == "1":
+if user_answer == "1":
     print("Sélectionnez la catégorie :")
     print(c.CATEGORIES_TO_RECOVER)
     category_selected = int(input())
     print("Catégorie sélectionnée : ", c.CATEGORIES_TO_RECOVER[category_selected])
     print("Sélectionnez l'aliment :")
-
-
-    #print("Sélectionnez l'aliment :")
-    #pour n aliments, afficher un chiffre de 1 à n devant chacun
-    #print("Aliment sélectionné : "yyyy"")
-    #query("""USE projet5;""")
-    #query("""SELECT * FROM product WHERE category = "xxxx" AND nutrition_grade_fr = 'a'
-    #    OR nutition_grade_fr = 'b' LIMIT 3""")
+    connection.query(f"""USE {c.DATABASE_NAME};""")
+    # la requête ci dessous fonctionne dans mysql. reste à la faire fonctionner depuis
+    #python et ajouter le filtre sur la catégorie sélectionnée
+    connection.query("""SELECT DISTINCT product_name FROM product
+        WHERE nutrition_grade_fr IN ('e', 'd') 
+        AND ORDER BY RAND() LIMIT 10;""")
 
 #User chooses 2:
-elif input() == "2":
+elif user_answer == "2":
     print("Mes aliments substitués :")
     #faire une query("""affichage de l'historique des aliments substitués""")
 
